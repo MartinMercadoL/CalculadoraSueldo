742cf632a164163ec9725dd4bb64424dc71b3287
MartinMercadoL <martin.080102@gmail.com>
2021-11-18 15:05:23 -0600

    Ya me salio

--- src/App.java
+++ src/App.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escribe tus horas de chamba");
-        var horas = sc.nextInt();
+        System.out.println("Ingresa horas trabajadas");
+        int horas = sc.nextInt();
 
+        if (horas <= 40) {
+            int sueldotot = horas * 16;
+            System.out.println("Tu sueldo es: " + sueldotot);
+        } else if (horas > 40) {
+            int horasextras = horas - 40;
+            int sueldoextra = horasextras * 20;
+            int sueldonormal = 40 * 16;
+            int sueldoT = sueldoextra + sueldonormal;
+            System.out.println("Tu sueldo con trabajo estra: " + sueldoT);
+        } else {
+            System.out.println("la cagetiaste");
+        }
+        sc.close();
     }
 }
